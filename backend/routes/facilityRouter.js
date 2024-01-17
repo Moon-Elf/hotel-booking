6baf472d8be6008ab4c1b18a07260b845a5e94e4
shz-code <web.shahidul.alam@gmail.com>
2024-01-18 00:41:40 +0600

    modify: backend (random room, facility response)

--- backend/routes/facilityRouter.js
+++ backend/routes/facilityRouter.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Facility = require("../models/facility");
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 router.route("/").get(async (req, res) => {
   try {
     const facilities = await Facility.find();
+    const randomizedArray = shuffleArray(facilities);
     setTimeout(() => {
-      return res.send(facilities);
+      return res.send(randomizedArray);
     }, 2000);
   } catch (err) {
     return res.status(400).status("Bad Request");
--- backend/routes/roomRouter.js
+++ backend/routes/roomRouter.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Room = require("../models/room");
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 router.route("/").get(async (req, res) => {
   try {
     const rooms = await Room.find();
+    const randomizedArray = shuffleArray(rooms);
     setTimeout(() => {
-      return res.send(rooms);
+      return res.send(randomizedArray);
     }, 2000);
   } catch (err) {
     return res.status(400).status("Bad Request");
