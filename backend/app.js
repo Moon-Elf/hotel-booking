222c7d9d0dcc5cf216808a86df383d87b332cc17
shz-code <web.shahidul.alam@gmail.com>
2024-01-17 18:17:42 +0600

    app: backend setup

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to HOTel");
+});
+
+module.exports = app;
