a43aeb2e0698333de2da6ba9587f0d7bb18b2714
shz-code <web.shahidul.alam@gmail.com>
2024-01-18 21:26:53 +0600

    add: frontend (list view rooms filtering, sorting added)

--- client/src/components/Home/LeftSidebar/Facilities.jsx
+++ client/src/components/Home/LeftSidebar/Facilities.jsx
@@ -1,9 +1,6 @@
-import { useSelector } from "react-redux";
 import Facility from "./Facility";
 
 export const Facilities = ({ data }) => {
-  const state = useSelector((state) => state.filter);
-  console.log(state);
   return (
     <div className="flex gap-2 flex-wrap">
       {data.map((item) => (
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         setMobileSidebarView={setMobileSidebar}
       />
       <div
-        className="flex gap-2 md:hidden bg-blue-100 w-fit px-3 py-2 m2-4 rounded-md cursor-auto"
+        className="flex gap-2 md:hidden bg-blue-100 w-fit px-3 py-2 m2-4 rounded-md cursor-pointer"
         onClick={toggleMobileView}
       >
         <Tags /> <h4>Filter by facilities</h4>
