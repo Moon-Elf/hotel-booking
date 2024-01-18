7801165e09ea58e3e3250ef4f0b1bb2305962d1c
shz-code <web.shahidul.alam@gmail.com>
2024-01-18 18:59:09 +0600

    refactor: frontend (Price tag show in list room)

--- client/src/components/Home/Main/ListRooms/ListRooms.jsx
+++ client/src/components/Home/Main/ListRooms/ListRooms.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
 import Room from "./Room";
 
 const ListRooms = ({ rooms }) => {
+  const { search, selectedRoomFacilities, sort } = useSelector(
+    (state) => state.filter
+  );
+
   return (
-    <div className="grid gap-4">
+    <div className="grid gap-4 mt-4">
       {rooms.map((room) => (
         <Room key={room._id} room={room} />
       ))}
--- client/src/components/Home/Main/ListRooms/Room.jsx
+++ client/src/components/Home/Main/ListRooms/Room.jsx
@@ -3,34 +3,46 @@ import { Link } from "react-router-dom";
 import Button from "../../../ui/Button";
 
 const Room = ({ room }) => {
-  const ratingArr = new Array(room.rating);
-  for (let i = 0; i < room.rating; i++) {
+  const {
+    name,
+    price,
+    url,
+    rating,
+    _id,
+    bookedCount,
+    bookingLimit,
+    desc,
+    facilities,
+  } = room;
+
+  const ratingArr = new Array(rating);
+  for (let i = 0; i < rating; i++) {
     ratingArr[i] = (i + 1).toString();
   }
-  const ratingLeftArr = new Array(5 - room.rating);
-  for (let i = 0; i < 5 - room.rating; i++) {
+  const ratingLeftArr = new Array(5 - rating);
+  for (let i = 0; i < 5 - rating; i++) {
     ratingLeftArr[i] = (i + 1).toString();
   }
   return (
     <div>
       <div className="listRoomCard flex gap-4 bg-slate-100 p-2 rounded-md">
-        <Link to="/" className="img-box">
-          <img src={room.url} alt={room.name} />
+        <Link to={`/room/${_id}`} className="img-box">
+          <img src={url} alt={name} />
         </Link>
         {/* Card Details */}
         <div className="listRoomCard-details flex flex-col justify-between w-full">
-          <h5 className="font-semibold text-lg">{room.name}</h5>
+          <h5 className="font-semibold text-lg">{name}</h5>
           {/* Card Body */}
           <div className="card-body grid gap-1 mt-2">
             <p>
-              {room.desc.length > 60 ? (
-                <span>{room.desc.substring(0, 60)}...</span>
+              {desc.length > 60 ? (
+                <span>{desc.substring(0, 60)}...</span>
               ) : (
-                room.desc
+                desc
               )}
             </p>
             <div className="flex flex-wrap gap-2 py-2">
-              {room.facilities.map((facility, index) => (
+              {facilities.map((facility, index) => (
                 <span
                   key={index}
                   className="bg-white px-2 rounded-md cursor-pointer flex gap-1 items-center"
@@ -38,6 +50,13 @@ const Room = ({ room }) => {
                   <Tags size={18} /> {facility}
                 </span>
               ))}
+            </div>
+            {/* Price */}
+            <div className="flex gap-2 items-center">
+              <span className="text-2xl font-bold text-orange-500">
+                {price}
+              </span>{" "}
+              BDT
             </div>
             <div className="flex gap-1">
               {ratingArr.map((arr, index) => (
@@ -54,16 +73,18 @@ const Room = ({ room }) => {
           </div>
           {/* Card Footer */}
           <div className="card-footer sm:flex justify-between items-center border-t mt-4 pt-2">
-            {room.bookedCount >= room.bookingLimit ? (
+            {bookedCount >= bookingLimit ? (
               <span className="font-semibold text-red-400">
                 Sorry! Running at maximum capacity
               </span>
             ) : (
-              <Button secondary msg="Book Today" />
+              <Link to={`/room/${_id}`}>
+                <Button secondary msg="Book Today" />
+              </Link>
             )}
             <div className="flex gap-1 justify-center sm:justify-end mt-2 sm:mt-0">
-              <span>{room.bookedCount}</span>/
-              <span className="font-bold">{room.bookingLimit}</span>
+              <span>{bookedCount}</span>/
+              <span className="font-bold">{bookingLimit}</span>
             </div>
           </div>
         </div>
