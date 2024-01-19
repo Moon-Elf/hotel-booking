98e794a05e1e86cfe0ba4089610c1bb7f482c884
shz-code <web.shahidul.alam@gmail.com>
2024-01-19 16:54:44 +0600

    add: frontend (auth feature)

---
+++ client/src/features/auth/authApi.js
@@ -0,0 +1,72 @@
+import { toast } from "react-hot-toast";
+import apiSlice from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "user",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              name: result.data.name,
+              email: result.data.email,
+            })
+          );
+          dispatch(
+            userLoggedIn({
+              name: result.data.name,
+              email: result.data.email,
+            })
+          );
+        } catch (err) {
+          toast.error(err.error.data);
+        }
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "user/login",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              name: result.data.name,
+              email: result.data.email,
+            })
+          );
+          dispatch(
+            userLoggedIn({
+              name: result.data.name,
+              email: result.data.email,
+            })
+          );
+        } catch (err) {
+          toast.error(err.error.data);
+        }
+      },
+    }),
+    checkPhone: builder.mutation({
+      query: (data) => ({
+        url: "user/check",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation, useCheckPhoneMutation } =
+  authApi;
