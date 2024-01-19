6e55651a889b990153750d8a27ff4cd4935037bc
shz-code <web.shahidul.alam@gmail.com>
2024-01-20 01:07:33 +0600

    add: frontend (sidebar self branding)

--- client/src/components/Home/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/Home/LeftSidebar/LeftSidebar.jsx
@@ -20,55 +20,66 @@ export default function LeftSidebar({
 
   return (
     <div
-      className={`w-3/4 md:w-1/2 p-4 bg-slate-100 rounded-md shadow-md
-    fixed md:sticky top-0 md:top-4 left-0 h-full md:h-fit transition-all md:translate-x-0 z-10 overflow-y-scroll md:overflow-y-hidden ${
-      !mobileSidebarView ? "-translate-x-full" : "translate-x-0"
-    }`}
+      className={`w-3/4 md:w-1/2 fixed bg-slate-100 md:bg-transparent md:sticky top-0 md:top-4 left-0 h-full md:h-fit transition-all md:translate-x-0 z-10 overflow-y-scroll md:overflow-y-hidden ${
+        !mobileSidebarView ? "-translate-x-full" : "translate-x-0"
+      }`}
     >
-      <div className="flex justify-between mb-4 pb-4 border-b">
-        <h2 className="font-semibold text-2xl">Facilities</h2>
-        {mobileSidebarView && (
-          <X
-            className="md:hidden"
-            size={30}
-            onClick={() => setMobileSidebarView(false)}
-          />
-        )}
+      <div className="bg-slate-100 md:rounded-md shadow-md p-4">
+        <div className="flex justify-between mb-4 pb-4 border-b">
+          <h2 className="font-semibold text-2xl">Facilities</h2>
+          {mobileSidebarView && (
+            <X
+              className="md:hidden"
+              size={30}
+              onClick={() => setMobileSidebarView(false)}
+            />
+          )}
+        </div>
+        {content}
+        <div className="socialLinks flex gap-2 mt-4 border-t pt-4">
+          <a
+            href="https://www.facebook.com/fb.shahidul.alam/"
+            className="bg-white p-2 rounded-full shadow-lg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Facebook />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/in-shahidul-alam/"
+            className="bg-white p-2 rounded-full shadow-lg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Linkedin />
+          </a>
+          <a
+            href="https://instagram.com/z_shanto_z"
+            className="bg-white p-2 rounded-full shadow-lg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Instagram />
+          </a>
+          <a
+            href="https://github.com/shz-code/"
+            className="bg-white p-2 rounded-full shadow-lg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github />
+          </a>
+        </div>
       </div>
-      {content}
-
-      <div className="socialLinks flex gap-2 mt-4 border-t pt-4">
+      <div className="text-black bg-slate-200 md:bg-transparent px-2 md:px-0 py-2 md:py-2">
+        Designed and developed by{" "}
         <a
-          href="https://www.facebook.com/fb.shahidul.alam/"
-          className="bg-white p-2 rounded-full shadow-lg"
+          className="font-bold"
+          href="https://shahidulalamcse.com"
           target="_blank"
           rel="noreferrer"
         >
-          <Facebook />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/in-shahidul-alam/"
-          className="bg-white p-2 rounded-full shadow-lg"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Linkedin />
-        </a>
-        <a
-          href="https://instagram.com/z_shanto_z"
-          className="bg-white p-2 rounded-full shadow-lg"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Instagram />
-        </a>
-        <a
-          href="https://github.com/shz-code/"
-          className="bg-white p-2 rounded-full shadow-lg"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Github />
+          Shahidul Alam
         </a>
       </div>
     </div>
